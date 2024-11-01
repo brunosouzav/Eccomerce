0ba4ade1344dd0778971e8a80e1d4e9fd5db25a7
Bruno Souza <brunocloud78@gmail.com>
2024-11-01 15:06:45 -0300

    Entidade e Repository produtos de afiliado, configuração banco de dados

---
+++ src/main/java/com/ProjetoPerformance/Ecommerce/entities/AffiliateProduct.java
@@ -0,0 +1,29 @@
+package com.ProjetoPerformance.Ecommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "affiliate_product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of ="id")
+public class AffiliateProduct {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	private String name;
+	private String description;
+	private String linkAffiliate;
+	private String imgUrl;
+}
---
+++ src/main/java/com/ProjetoPerformance/Ecommerce/repositories/AffiliateProductRepository.java
@@ -0,0 +1,7 @@
+package com.ProjetoPerformance.Ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ProjetoPerformance.Ecommerce.entities.AffiliateProduct;
+public interface AffiliateProductRepository extends JpaRepository<AffiliateProduct, Long>{
+
+}
